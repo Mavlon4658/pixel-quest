a23a5cac51f997630239677b529fbd5dc20bacab
Mavlon4658 <72940597+Mavlon4658@users.noreply.github.com>
2023-12-12 23:31:53 +0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <div class="inputs">
+      <Vue3Signature></Vue3Signature>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue3Signature from "vue3-signature"
+export default {
+  name: 'App',
+  components: {
+    Vue3Signature
+  }
+}
+</script>
+
+<style>
+.app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.inputs {
+  border: 1px solid red;
+  width: 700px;
+  height: 500px;
+}
+</style>
